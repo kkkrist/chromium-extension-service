b1c3cfc8d3f67f56352904b133400d2f67a53faf
Christian Mund <git@mundpropaganda.net>
2019-11-24 21:50:09 +0100

    api: use Promise.allSettled and filter out unfulfilled promises from failed fetch requests (#5)
    * npm: use node v12.x
    * api: use Promise.allSettled and filter out unfulfilled promises from failed fetch requests

--- api/index.js
+++ api/index.js
@@ -117,7 +117,7 @@ module.exports = async (req, res) => {
       }, {})
 
     const fresh = _flatten(
-      await Promise.all(
+      await Promise.allSettled(
         Object.keys(jobs).map(
           updateUrl =>
             updateUrl &&
@@ -126,9 +126,16 @@ module.exports = async (req, res) => {
       )
     )
 
-    return res
-      .status(200)
-      .json([...cached, ...(await updateCache(fresh, prodversion))])
+    return res.status(200).json([
+      ...cached,
+      ...(await updateCache(
+        fresh
+          .filter(({ status }) => status === 'fulfilled')
+          .map(({ value }) => value)
+          .flat(),
+        prodversion
+      ))
+    ])
   } catch (error) {
     console.error(error, req.body)
     return res.status(500).json({ error: error.message })
